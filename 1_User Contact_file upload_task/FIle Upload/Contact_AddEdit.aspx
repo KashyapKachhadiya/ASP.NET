b3cd5e17dd845344d3988f80a9957447efdf7ff9
Kashyap Kachhadiya <kashyap1572004@gmail.com>
2024-09-14 18:42:12 +0530

    Update Contact Add Edit LIst

---
+++ 1_User Contact_file upload_task/FIle Upload/Contact_AddEdit.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact_AddEdit.aspx.cs" Inherits="WebApplication1.FIle_Upload.Contact_AddEdit" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <h1>Contact Add Edit Form</h1>
+        <p>&nbsp;</p>
+        <p>
+            <asp:Label ID="lblMsg" runat="server" EnableViewState="False"></asp:Label>
+        </p>
+        <p>&nbsp;</p>
+        <p>Contact Name : <asp:TextBox ID="txtContactName" runat="server"></asp:TextBox>
+        </p>
+        <p>&nbsp;</p>
+        <p>Upload Photo : <asp:FileUpload ID="fuContactPhoto" runat="server" />
+        </p>
+        <p>&nbsp;</p>
+        <p>Contact Category :
+            <asp:CheckBoxList ID="cblContactCategory" runat="server">
+            </asp:CheckBoxList>
+        </p>
+        <p>&nbsp;</p>
+        <p>
+            <asp:Button ID="btnSave" runat="server" BackColor="#000000" ForeColor="White" OnClick="btnSave_Click" Text="Save" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ 1_User Contact_file upload_task/FIle Upload/Contact_AddEdit.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlTypes;
+
+namespace WebApplication1.FIle_Upload
+{
+    public partial class Contact_AddEdit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                FillCheckBoxList();
+            }
+        }
+
+        private void FillCheckBoxList ()
+        {
+            SqlConnection objConn = new SqlConnection();
+            objConn.ConnectionString = "data source=DESKTOP-ELGAHLN;  initial catalog=MSTDATA;  Integrated Security=True;";
+
+            try
+            {
+                objConn.Open ();
+                //Read the Connection String from web.config file objConn.Open();
+                SqlCommand objCmd = objConn.CreateCommand();
+                objCmd.CommandType = CommandType.StoredProcedure;
+                objCmd.CommandText = "Pr_ContactCategory_SelectForDropDownList";
+                SqlDataReader objSDR = objCmd.ExecuteReader();
+                if (objSDR.HasRows)
+                {
+                    cblContactCategory.DataTextField = "ContactCategoryName";
+                    cblContactCategory. DataValueField = "ContactCategoryID";
+                    cblContactCategory.DataSource = objSDR;
+                    cblContactCategory.DataBind();
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write("SQL Error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+            finally
+            {
+                objConn.Close();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+            String ContactPhotoPath = "";
+            if (fuContactPhoto.HasFile)
+            {
+
+                ContactPhotoPath = "~/UserContent/" + fuContactPhoto.FileName.ToString().Trim();
+                fuContactPhoto.SaveAs(Server.MapPath(ContactPhotoPath));
+            }
+
+            SqlConnection Objconn = new SqlConnection();
+            Objconn.ConnectionString = "data source=DESKTOP-ELGAHLN;  initial catalog=MSTDATA;  Integrated Security=True;";
+            try
+            {
+                Objconn.Open();
+
+
+                SqlCommand cmdObj = Objconn.CreateCommand();
+
+                cmdObj.CommandType = CommandType.StoredProcedure;
+
+                cmdObj.CommandText = "Pr_Contact_Insert";
+
+
+                cmdObj.Parameters.AddWithValue("@ContactName", txtContactName.Text.Trim());
+                cmdObj.Parameters.AddWithValue("@ContactPhotoPath", ContactPhotoPath);
+
+                //Out Parameter
+                cmdObj.Parameters.Add("@ContactID", SqlDbType.Int, 4). Direction = ParameterDirection.Output; 
+                //objCmd.Parameters["@ContactID"].Direction ParameterDirection.Output;
+
+                cmdObj.ExecuteNonQuery();
+
+                SqlInt32 ContactID = 0;
+                ContactID = Convert.ToInt32(cmdObj.Parameters["@ContactID"].Value);
+
+                //We need ContactID (PK) after insertion of the record
+                foreach (ListItem liContactCategoryID in cblContactCategory.Items)
+                {
+                    if (liContactCategoryID.Selected)
+                    {
+                        SqlCommand objCmdContactCategory = Objconn.CreateCommand();
+                        //objCmdContactCategory.CommandType = CommandType.StoredProcedure;
+                        //objCmdContactCategory.CommandText = "Pr_ContactWiseCategory_Insert";
+                        //objCmdContactCategory.Parameters.AddWithValue("ContactID", ContactID.ToString());
+                        ////objCmdContactCategory.Parameters.AddWithValue("@ContactCategoryID", liContactCategoryID.Value.ToString());
+                        ////objCmdContactCategory.ExecuteNonQuery();
+                        ///SqlCommand objCmdContactCategory = Objconn.CreateCommand();
+                        objCmdContactCategory.CommandType = CommandType.StoredProcedure;
+                        objCmdContactCategory.CommandText = "Pr_ContactWiseCategory_Insert";
+
+                        // Pass the correct ContactID (PK) from previous insert
+                        objCmdContactCategory.Parameters.AddWithValue("@ContactID", ContactID);
+
+                        // Pass the selected ContactCategoryID
+                        objCmdContactCategory.Parameters.AddWithValue("@ContactCategoryID", liContactCategoryID.Value);
+
+                        // Execute the insert command for each selected category
+                        objCmdContactCategory.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write("SQL Error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+            finally
+            {
+
+                Objconn.Close();
+                lblMsg.Text = "Uploaded Successfully...";
+
+                txtContactName.Text = "";
+
+                txtContactName.Focus();
+            }
+        }
+    }
+}
